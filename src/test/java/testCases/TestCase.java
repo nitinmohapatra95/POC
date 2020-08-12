6a266857f7e58e0a30032bd23cff96aae8e398af
Mohapatra <nmohapatra@informatica.com>
2020-08-12 12:54:22 +0530

    My first commit

---
+++ src/test/java/testCases/TestCase.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package testCases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author nmohapat
+ *
+ */
+public class TestCase {
+
+	@Test
+	void test() {
+		System.out.println("Hello World");
+	}
+}
